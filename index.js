cda2bcda61c3194e2f828adf5b9888870afdfb74
unknown <vohoangduc3012@gmail.com>
2024-12-04 15:26:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const expressHbs = require("express-handlebars");
 const { createPagination } = require("express-handlebars-paginate");
 

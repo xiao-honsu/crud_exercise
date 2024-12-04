5697c2aceab5041468f92464a6bc0879dad39ac4
unknown <vohoangduc3012@gmail.com>
2024-12-04 15:12:26 +0700

    upload

---
+++ routes/userRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/userController");
+
+router.get("/", controller.show);
+router.post("/", controller.addUser);
+router.put("/", controller.editUser);
+router.delete("/:id", controller.deleteUser);
+module.exports = router;
